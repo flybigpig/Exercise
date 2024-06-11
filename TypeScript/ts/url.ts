8d82dbbed25b5bd0de457bc165b7d90458da2a96
02231406 <wangkai@yto.net.cn>
2024-06-11 13:35:13 +0800

    update

--- TypeScript/ts/url.ts
+++ TypeScript/ts/url.ts
@@ -2,6 +2,13 @@
 
 var url = "https://mobilekh.yto.net.cn/#/helpCenter?token=BearereyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhM2Y4MmQ0YWI4NjE0MzAzYTlkYzI4ZjU0N2JhNDNiNCIsImNyZWF0ZWQiOjE3MTc0Nzc2Mzk2NDcsImV4cCI6MTcyMDA2OTYzOX0.Up4Ix4z4BzvCIwhJHhWyvs9uKOGIXf8X7JekbjDwjnVu_xSvRiKJLiFJGvsI4zka0hG28UIZFQDnxGppnOrbBA&userCode=a3f82d4ab8614303a9dc28f547ba43b4&version=73.2.0.1"
 
+
+url = "https://mobilekh.yto.net.cn/#/helpCenter?&token=BearereyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhM2Y4MmQ0YWI4NjE0MzAzYTlkYzI4ZjU0N2JhNDNiNCIsImNyZWF0ZWQiOjE3MTc0NzkxNzE3OTQsImV4cCI6MTcyMDA3MTE3MX0.GFNsS_G9J981LUZnbYV8daKwSRI3-2lSLsysMu201tOV0w5VMl74Nv49e4B2bEuJ_vPT3X_-uJWONt-gMw74xA&userCode=a3f82d4ab8614303a9dc28f547ba43b4&version=1.0.0"
+
+
+url = "https://mobilekh.yto.net.cn/#/problemOrder/index?userCode=a3f82d4ab8614303a9dc28f547ba43b4&token=BearereyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhM2Y4MmQ0YWI4NjE0MzAzYTlkYzI4ZjU0N2JhNDNiNCIsImNyZWF0ZWQiOjE3MTc0Nzc2Mzk2NDcsImV4cCI6MTcyMDA2OTYzOX0.Up4Ix4z4BzvCIwhJHhWyvs9uKOGIXf8X7JekbjDwjnVu_xSvRiKJLiFJGvsI4zka0hG28UIZFQDnxGppnOrbBA&groupId=2608799&phone=18392391572"
+
+url = "https://mobilekh.yto.net.cn/#/indicatorDescription/index?token=BearereyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhM2Y4MmQ0YWI4NjE0MzAzYTlkYzI4ZjU0N2JhNDNiNCIsImNyZWF0ZWQiOjE3MTc0Nzc2Mzk2NDcsImV4cCI6MTcyMDA2OTYzOX0.Up4Ix4z4BzvCIwhJHhWyvs9uKOGIXf8X7JekbjDwjnVu_xSvRiKJLiFJGvsI4zka0hG28UIZFQDnxGppnOrbBA&version=73.2.0.1"
 function urlQueryToArr( url :string) {
     const arr = url.split('?')[1].split('&');
    return arr
--- TypeScript/ts/zhengce.ts
+++ TypeScript/ts/zhengce.ts
@@ -5,6 +5,33 @@ var b= 'ts'
 var a= 'aaaaaaaaatsaaaaa'
 
 
-let result  =a.match(b)
+// let result  =a.match(b)
 
-console.log (result?.index)
+// console.log (result?.index)
+
+
+let reg = "[\u4e00-\u9fa5]{0,8}";
+var str = "w我爱你asd"
+console.log (str.match(reg))
+
+
+// let regg = "^[a-zA-Z0-9]{0,16}"
+// var str = "asdasdasdasdasdasd唉"
+// console.log (str.match(regg))
+
+let reggg = "^[a-zA-Z0-9\u4e00-\u9fa5]{0,16}"
+var str = "w我爱你asd"
+console.log (str.match(reggg))
+
+function checkName(psw: string): boolean {
+    let reg = "^[\u4e00-\u9fa5]{0,8}"
+    let regg = "^[a-zA-Z0-9]{0,16}"
+    let reggg = "^[a-zA-Z0-9\u4e00-\u9fa5]{0,16}"
+    let result1 = psw.match(reg)
+    let result2 = psw.match(regg)
+    let result3 = psw.match(reggg)
+    console.log("result1 " + result1?.[0]  );
+    console.log("result2 " + result2?.[0] );
+    console.log("result3 " + result3?.[0] );
+    return false
+  }
