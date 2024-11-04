29ff1652ecc9705d40bda17c195d94db00583de1
02231406 <wangkai@yto.net.cn>
2024-11-04 13:13:21 +0800

    update

--- DataStructC/struct/SqList.c
+++ DataStructC/struct/SqList.c
@@ -2,24 +2,24 @@
 // Created by YT_FLY on 2024/5/7.
 //
 
-#define  LIST_INIT_SIZE 1000  // 基础容量
+#define LIST_INIT_SIZE 1000 // 基础容量
 
-#define LISTINCREMENT  50   // 分配增量
-
+#define LISTINCREMENT 50 // 分配增量
 
 #include <stdio.h>
 #include <string.h>
 
-
-struct Books {
+struct Books
+{
     char title[50];
     char author[50];
     char subject[100];
     int book_id;
 } book = {"C 语言", "RUNOOB", "编程语言", 123456};
 
-typedef struct {
-//    ElemType *elem;
+typedef struct
+{
+    //    ElemType *elem;
     char site[50];
     int length;
     int listSize;
@@ -27,13 +27,22 @@ typedef struct {
 
 } SqList;
 
-int main() {
+int main()
+{
     SqList sqlist;
+
     sqlist.length = 1;
     sqlist.listSize = 1;
+    sqlist.books = book;
 
-    strcpy( sqlist.site, "C Programming");
+    strcpy(sqlist.site, "C Programming");
     printf("%d\n", sqlist.listSize);
+
+    printf("%d\n", sqlist.books.book_id);
+
+    printf("%s\n", sqlist.books.title);
+    printf("%s\n", sqlist.books.author);
+    printf("%s\n", sqlist.books.subject);
 
     printf("%s\n", sqlist.site);
     return 0;
